b5c63922ed5b4f5624f00d8b76ae94a168f6a8b2
Aryan Godara <aryangodara03@gmail.com>
2023-11-02 15:22:23 +0530

    add argument for chainlink aggregatorv3 interface
    Signed-off-by: Aryan Godara <aryangodara03@gmail.com>

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -8,7 +8,7 @@ contract DeployFundMe is Script {
     function run() external payable {
         vm.startBroadcast();
         new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        // Address found on : https://docs.chain.link/data-feeds/api-reference
+        // Address found on : https://docs.chain.link/data-feeds/api-reference (Google: `chainlink aggregatorv3interface address`)
         vm.stopBroadcast();
     }
 }
